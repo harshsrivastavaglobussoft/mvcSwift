28fee2c94c6799b8b9dae22185afda6377f482e5
harshsrivastava_globussoft <harshsrivastava@globussoft.in>
2017-08-10 19:09:55 +0530

    download manager

---
+++ DownloadManager/DownloadManager.swift
@@ -0,0 +1,13 @@
+//
+//  DownloadManager.swift
+//  DribbleInSwift
+//
+//  Created by Sumit Ghosh on 10/08/17.
+//  Copyright © 2017 Sumit Ghosh. All rights reserved.
+//
+
+import UIKit
+
+class DownloadManager: NSObject {
+
+}
